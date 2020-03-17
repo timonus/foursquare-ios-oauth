58734c3698d9ab750f6728cd6d8aebf80e470901
Tim Johnsen <timonus@gmail.com>
2020-03-16 22:28:12 -0700

    Add comments to the header.

--- TJFoursquareAuthentication.h
+++ TJFoursquareAuthentication.h
@@ -11,11 +11,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface TJFoursquareAuthentication : NSObject
 
+/**
+* Invoke this to initiate auth
+* @param clientIdentifier Your registered Foursquare client identifier.
+* @param clientIdentifier Your registered Foursquare redirect URI.
+* @param clientIdentifier Foursquare client secret.
+* @param completion Block invoked when auth is complete. @c accessToken will be @c nil if auth wasn't completed.
+*/
 + (void)authenticateWithClientIdentifier:(NSString *const)clientIdentifier
                              redirectURI:(NSURL *const)redirectURI
                             clientSecret:(NSString *const)clientSecret
                               completion:(void (^)(NSString *_Nullable accessToken))completion API_AVAILABLE(ios(8.0));
 
+/// Invoke this from your app delegate's implementation of -application:openURL:options:, returns whether or not the URL was a completion callback to Foursquare auth.
 + (BOOL)tryHandleAuthenticationCallbackWithURL:(NSURL *const)url API_AVAILABLE(ios(8.0));
 
 @end
